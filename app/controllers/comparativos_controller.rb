50eb29c22e1aad7bf4a82f9d3d500ce8528444f4
Felipe Arosa <felipe.arosa@gmail.com>
2022-09-23 17:38:40 -0300

    chose siglas

--- app/controllers/comparativos_controller.rb
+++ app/controllers/comparativos_controller.rb
@@ -1,6 +1,15 @@
 class ComparativosController < ApplicationController
   def show
+    @senador_primary_id = params[:senador1]
+    @senador_secondary_id = params[:senador2]
 
+    unjsoned_votes_primary = URI.open("https://legis.senado.leg.br/dadosabertos/senador/#{@senador_primary_id}/votacoes.json").read
+    @votes_primary = JSON.parse(unjsoned_votes_primary)
+    @siglas = @votes_primary["VotacaoParlamentar"]["Parlamentar"]["Votacoes"]["Votacao"].map do |votacao|
+      votacao["Materia"]["Sigla"]
+    end
+    @final_siglas = @siglas.uniq { |sigla| sigla }
+    raise
   end
 
 end

9c7f8fb239974b8bafc769f958c68d02d8150531
Felipe Arosa <felipe.arosa@gmail.com>
2022-09-27 17:57:37 -0300

    counted all responses, yes and nos

--- app/javascript/controllers/comparison_table_controller.js
+++ app/javascript/controllers/comparison_table_controller.js
@@ -2,11 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="comparison-table"
 export default class extends Controller {
-  static targets = ["lineComparison", "voted", "noRecord"]
+  static targets = ["lineComparison", "votedPrimary","votedSecondary", "noRecord", "votedTogether"]
 
   connect() {
-    console.log(this.noRecordTargets)
+    this.#checkSameness(this.lineComparisonTargets);
+
+    const votedSame = this.#countAll(this.lineComparisonTargets, 'same-choice');
+    const votedDiff = this.#countAll(this.lineComparisonTargets, 'diff-choice');
+
+    console.log(votedSame);
+    console.log(votedDiff);
+
   }
 
+  #checkSameness(arrLines){
+    arrLines.forEach((tr) => {
+      const primChoice = tr.querySelector('.primary-choice').innerHTML;
+      const secChoice = tr.querySelector('.secondary-choice').innerHTML;
+
+      if (primChoice === secChoice) {
+        tr.classList.add('same-choice');
+      } else if((primChoice === 'Sim' || primChoice === 'Não') && (secChoice === 'Sim' || secChoice === 'Não')){
+        tr.classList.add('diff-choice')
+      }
+    });
+  }
+
+
+  #countAll(arrLines, className){
+    let count = 0
+    arrLines.forEach(tr => {
+      if(tr.className.includes(className)) { count +=1 }
+    });
+    return count;
+  }
 
 }

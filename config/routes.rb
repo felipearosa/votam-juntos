52b72ed4ddf5c20a7d4e8fe2787b7ed335ed9d71
Felipe <felipe.arosa@gmail.com>
2022-11-24 20:39:03 -0300

    removed sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  require "sidekiq/web"
-  # //authenticate :user, ->(user) { user.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  # end
-
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

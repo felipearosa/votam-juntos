357c21d5b10d4404b8b1d5f06902dcbb4e6d06d1
Felipe <felipe.arosa@gmail.com>
2022-11-26 23:15:26 -0300

    added description to the comparison table

--- app/javascript/controllers/bills_table_controller.js
+++ app/javascript/controllers/bills_table_controller.js
@@ -1,23 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import { showMoreBox } from "../helpers/show_more_box"
 
 // Connects to data-controller="bills-table"
 export default class extends Controller {
   static targets = ["descriptionCell","voteDescription"]
 
   connect() {
-    this.descriptionCellTargets.forEach(descriptionCell => {
-      const voteDescription = descriptionCell.querySelector('.position');
-      const hoverText = descriptionCell.querySelector('.hover-text');
-
-      descriptionCell.addEventListener('mouseover', () => {
-        voteDescription.classList.remove("inactive");
-        hoverText.classList.add("hover-text-active");
-      })
-
-      descriptionCell.addEventListener('mouseout', () => {
-        voteDescription.classList.add("inactive");
-        hoverText.classList.remove("hover-text-active");
-      })
-    });
+    showMoreBox(this.descriptionCellTargets);
   }
 }
